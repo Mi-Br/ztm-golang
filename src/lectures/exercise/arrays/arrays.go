db5949cf6429f9435714f140817fe8a3622ce7dd
Michail <michailbredichin@gmail.com>
2025-06-22 12:45:10 +0200

    arrays

--- src/lectures/exercise/arrays/arrays.go
+++ src/lectures/exercise/arrays/arrays.go
@@ -18,7 +18,29 @@ package main
 
 import "fmt"
 
-func main() {
-
+type Product struct {
+	Name  string
+	Price float32
 }
 
+func main() {
+	shoppingList := [4]Product{}
+	shoppingList[0] = Product{Name: "Apple", Price: 3.33}
+	shoppingList[1] = Product{Name: "Banana", Price: 2.99}
+	shoppingList[2] = Product{Name: "Orange", Price: 3.33}
+	fmt.Println("last item:\t", shoppingList[len(shoppingList)-1])
+	fmt.Println("total items:\t", len(shoppingList))
+	fmt.Println("total cost of items:\t", calculateTotal(shoppingList[:]))
+	shoppingList[3] = Product{Name: "Tomatoes", Price: 4.44}
+	fmt.Println("total items:\t", len(shoppingList))
+	fmt.Println("total cost of items:\t", calculateTotal(shoppingList[:]))
+}
+func calculateTotal(p []Product) float32 {
+	var out float32 = 0
+
+	for _, item := range p {
+		out += item.Price
+	}
+
+	return out
+}
